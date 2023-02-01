afbb43dea3cc19685c6fe313a9aa22e792f6b1ec
Francesco Orazini <horace.supertramp@gmail.com>
2023-02-01 12:43:25 +0100

    Add basic index layout

--- resources/views/countries/index.blade.php
+++ resources/views/countries/index.blade.php
@@ -1,16 +1,52 @@
 @extends('layouts.base')
 
 @section('content')
-    <div class="content">
-        <div class="title m-b-md">
-            Laravel
+    <div class="container">
+        <h1>Countries</h1>
+        <div class="row">
+            @foreach($countries as $country)
+            <div class="col-lg-4 col-md-6 col-sm-12 gy-3">
+                <div class="card h-100">
+                    <div class="card-body pb-0">
+                        <h5 class="card-title">
+                            <a href="{{ route('countries.show', [ "country" => $country ]) }}" class="card-link">{{ $country->name }}</a>
+                            <small class="text-muted">({{ $country->code }})</small>
+                        </h5>
+                      <h6>{{ $country->local_name }}</h6>
+                    </div>
+                    <div class="card-body pt-0">
+                        <table class="table table-sm mb-0">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Continent</th>
+                                    <td>{{ $country->continent }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Region</th>
+                                    <td>{{ $country->region }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Surface</th>
+                                    <td>{{ $country->surface_area }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Population</th>
+                                    <td>{{ $country->population }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Life Expectancy</th>
+                                    <td>{{ $country->life_expectancy }}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Government form</th>
+                                    <td>{{ $country->government_form }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
-        <ul>
-        @foreach($countries as $country)
-        <a href="{{ route('countries.show', ['country' => $country]) }}">
-            <li>{{ $country->name }}</li>
-        </a>
-        @endforeach
-        </ul>
     </div>
 @endsection
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Geo</title>
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
+    <script src="{{ asset("js/app.js") }}" async defer></script>
 </head>
 <body>
     <main>
